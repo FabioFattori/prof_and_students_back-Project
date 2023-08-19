3c046ad712bf921db5fe06360237450ef409955a
Fabio Fattori <fabiofattoriw@gmail.com>
2023-08-19 17:27:42 +0200

    Update getAllStudentsOfAProf.php

--- Api/getAllStudentsOfAProf.php
+++ Api/getAllStudentsOfAProf.php
@@ -1,13 +1,30 @@
 <?php
-
-include "../Class/DB.php";
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-$idProd=$_GET['prof_id'] ?? 1;
+include "../Class/DB.php";
 
-//get all the students of a prof, where ID_Profs contains the id of the prof
-$sql = "SELECT * FROM `students` WHERE `ID_Profs` LIKE ?";
 
-echo json_encode(PDOCommands::get_rows($sql,$idProd));
+$id=$_GET['id'] ?? null;
+
+if($id == null)
+{
+  throw new Exception("id is required");
+
+  die();
+}
+
+$arrStud=PDOCommands::get_rows("select ID_Studente from voto where ID_Prof = ?",$id);
+
+//make every element unique
+$arrStud=array_unique($arrStud,SORT_REGULAR);
+
+$toReturn=[];
+
+foreach($arrStud as $singleStud)
+{
+    $toReturn=array_merge($toReturn,PDOCommands::get_rows("select * from studente where id=?",$singleStud['ID_Studente']));
+}
+
+echo json_encode($toReturn);
